6010e5d2782de4cd700ac7c9faf75f678aadf7a5
Guilherme Ramos <guiih.ramos22@gmail.com>
2023-05-30 22:28:44 -0300

    feat: forms school final

--- lib/src/schools/school_form_page.dart
+++ lib/src/schools/school_form_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:safe_schools/src/schools/school.dart';
 import 'package:safe_schools/src/schools/schools_repository.dart';
+import 'package:safe_schools/src/shared/states.dart';
 
 import '../shared/enums/states.dart';
 
