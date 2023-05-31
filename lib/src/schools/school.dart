79ac678ff9fe7d1087447de69d0fc4f2697db054
Guilherme Ramos <guiih.ramos22@gmail.com>
2023-05-30 22:28:44 -0300

    feat: forms school final

--- lib/src/schools/school.dart
+++ lib/src/schools/school.dart
@@ -1,37 +1,38 @@
 import 'dart:convert';
 
 class School {
-  final int id;
+  final int? id;
   final String name;
   final String street;
   final String number;
   final String neighborhood;
-  final String complement;
+  final String? complement;
   final String city;
   final String state;
 
   School(
-      {required this.id,
+      {this.id,
       required this.name,
       required this.street,
       required this.number,
       required this.neighborhood,
-      required this.complement,
+      this.complement,
       required this.city,
       required this.state});
 
   factory School.fromJson(Map<String, dynamic> json) {
     return School(
-      id: json['id'],
+      id: json['id'] ?? '',
       name: json['name'],
       street: json['street'],
       number: json['number'],
       neighborhood: json['neighborhood'],
-      complement: json['complement'],
+      complement: json['complement'] ?? '',
       city: json['city'],
       state: json['state'],
     );
   }
+
   String toJson() {
     return jsonEncode({
       'id': id,
