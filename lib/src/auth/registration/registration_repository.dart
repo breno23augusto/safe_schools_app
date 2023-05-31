6163757f9df32041fc3a471e305744d056443485
Pedro Augusto Santana <pedroaugustoansantana@gmail.com>
2023-05-30 22:36:40 -0300

    chore: renamed ConstConfig → ConstConfigs

--- lib/src/auth/registration/registration_repository.dart
+++ lib/src/auth/registration/registration_repository.dart
@@ -6,7 +6,7 @@ import 'package:http/http.dart' as http;
 import 'package:safe_schools/src/shared/settings/const_configs.dart';
 
 class RegistrationRepository {
-  static const String _apiBasePath = ConstConfig.apiUrl;
+  static const String _apiBasePath = ConstConfigs.apiUrl;
   static const String _authTokenKey = 'auth_token';
 
   final _storage = const FlutterSecureStorage();
